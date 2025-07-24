020d42de3c04520cd8cd27609d2cf52c26801116
Joziane Oliveira <jozioliveira@edu.uniube.br>
2025-07-24 00:14:19 -0300

    Initial commit from Create Next App

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Joziane
+            <span className="text-accent">.</span>
+            Oliveira
+          </h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
