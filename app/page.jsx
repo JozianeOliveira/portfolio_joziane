f8aac3b67df9c1a1726559289bce1ea7c4ad51ec
Joziane Oliveira <jozioliveira@edu.uniube.br>
2025-07-28 22:02:51 -0300

    feat: adiciona estilos para h1, h2 e h3 com Tailwind CSS  feat: adiciona seção de introdução com nome e descrição  style: aplica classes utilitárias ao cabeçalho e parágrafo  feat: cria seção com título, subtítulo e descrição estilizada

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,15 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div>text</div>
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Analista de Sistemas</span>
+            <h1 className="h1">
+              Hello, I'm <br /><span className="text-accent">Joziane Oliveira</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia culpa, deserunt odit sunt, libero cum architecto placeat temporibus reiciendis ipsam ipsum. Laborum tempora voluptate rem. Aperiam vel quis sed laudantium!
+            </p>
+          </div>
           {/* photo */}
           <div>photo</div>
         </div>
